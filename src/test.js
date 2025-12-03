09b6e02e51503cca891c8693aa9c2914d0fb9212
Emin Beköz <m.eminbekoz19@gmail.com>
2025-12-03 22:32:25 +0300

    Add Test component with basic structure and styling integration

---
+++ src/test.js
@@ -0,0 +1,13 @@
+import styles from './App.module.css';
+
+function Test() {
+
+    return (
+        <div className={styles.Test}>
+            <h1>Test componenti</h1>
+        </div>
+    )
+}
+
+
+export default Test;
