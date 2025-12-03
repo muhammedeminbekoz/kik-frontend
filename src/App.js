e0a7aef4334b6679c241b1e89d313b250729d099
Emin Beköz <m.eminbekoz19@gmail.com>
2025-12-03 22:32:15 +0300

    Enhance App component with environment checks and styling; integrate Test component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
-
+import { useEffect } from 'react';
+import Test from './test';
+import styles from './App.module.css';
 function App() {
+
+
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'development') {
+      //eğer development'sa bişeyler bişeyler yap
+    }
+  })
+
   return (
-    <div className="App">
-      test
+
+    <div className={styles.App} >
+
+
+      <h3> {process.env.NODE_ENV} </h3>
+      <p> {process.env.REACT_APP_API_URL} </p>
+      <Test />
     </div>
   );
 }
